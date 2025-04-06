bdc318d961f556e75c83f757f60cbdf4f7d1e0ad
dm <dmitry@udalov.online>
2025-04-07 05:02:29 +0600

    add amd kargs

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,14 +2,9 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
 dnf5 copr enable -y ilyaz/LACT
-#dnf5 update -y
 dnf5 install -y mc firefox lact
 systemctl enable lactd
+
+touch /usr/lib/bootc/kargs.d/10-amd-gpu.toml
+echo "kargs = [\"amdgpu.ppfeaturemask=0xffffffff\"]" > /usr/lib/bootc/kargs.d/10-amd-gpu.toml
