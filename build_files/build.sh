7185f9b7825b8f7836be8e557bb725cd5ce14af5
dm <dmitry@udalov.online>
2025-04-28 15:16:58 +0600

    add disable zenpower3

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,8 +3,7 @@
 set -ouex pipefail
 
 dnf5 copr enable -y ilyaz/LACT
-dnf5 copr enable -y shdwchn10/zenpower3
-dnf5 install -y mc firefox lact zenpower3 zenmonitor3
+dnf5 install -y mc firefox lact
 systemctl enable lactd
 
 touch /usr/lib/bootc/kargs.d/10-amd-gpu.toml
