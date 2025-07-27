6b901f8f5e2e3d5264d5e8ee86c7ba1403e6b5e0
dm <dmitry@udalov.online>
2025-07-27 21:25:08 +0600

    fix amd gpu

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,4 +7,6 @@ dnf5 install -y mc firefox lact NetworkManager-libreswan-gnome
 systemctl enable lactd
 
 touch /usr/lib/bootc/kargs.d/10-amd-gpu.toml
+touch /usr/lib/bootc/kargs.d/11-amd-gpu-fix-artefact.toml
 echo "kargs = [\"amdgpu.ppfeaturemask=0xffffffff\"]" > /usr/lib/bootc/kargs.d/10-amd-gpu.toml
+echo "kargs = [\"amdgpu.dcdebugmask=0x10\"]" > /usr/lib/bootc/kargs.d/11-amd-gpu-fix-artefact.toml
